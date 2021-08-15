8dd899798497308ae382552bea4722e85c648a88
Dave <dave.cadelina@outlook.com>
2021-08-15 10:11:42 +1000

    removed resume and changed to view projects

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,5 +2,5 @@ export function addResume(pdf) {
   if (!pdf) return;
 
   const resumeButton = document.querySelector(".cta-btn--resume");
-  resumeButton.setAttribute("href", pdf);
+  //resumeButton.setAttribute("href", pdf);
 }
